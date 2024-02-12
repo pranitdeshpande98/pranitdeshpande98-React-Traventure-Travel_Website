f6194c4e9f9d533270dc0e794d357355f95cf4dc
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-02-12 13:51:13 -0500

    Preliminary UI changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import "./NavbarStyles.css";
+import { Component } from "react";
+import { MenuItems } from "./MenuItems"; 
+
+class Navbar extends Component {
+state = {clicked: false};
+
+handleclick = () => {
+    this.setState({ clicked: !this.state.clicked })
+}
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo"> Traventure </h1>
+        <div className="menu-icons" onClick={this.handleclick}>
+            <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+            {MenuItems.map((item,index) => {
+                return (
+                    <li key={index}> 
+                    <a className={item.cName} href="/">
+                       <i className={item.icon}></i> {item.title} 
+                    </a> 
+                    </li>
+                )
+            })}
+           <button>Sign Up</button>
+        </ul>
+     </nav>
+    )
+  }
+}
+
+export default Navbar
